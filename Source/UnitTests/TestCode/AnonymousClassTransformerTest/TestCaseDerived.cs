5f41788eab7b06555440aa92dde8ad1a5bec5f6c
mehdimo <mehdi.mka@gmail.com>
2007-02-28 12:18:01 +0000

    CleanUp: some test code files committed by error in past
    git-svn-id: https://lpms:444/svn/janett2/trunk@46 b1315daf-0e9f-db11-9a89-001422107726

--- Source/UnitTests/TestCode/AnonymousClassTransformerTest/TestCaseDerived.cs
+++
@@ -1,8 +0,0 @@
-namespace Test
-{
-	using junit.framework.TestCase;
-	public abstract class AbstractWords : TestCase
-	{
-		public void Method(){}
-	}
-}
--- Source/UnitTests/TestCode/AnonymousClassTransformerTest/TestCaseDerived.java
+++
@@ -1,6 +0,0 @@
-package Test
-import junit.framework.TestCase;
-public abstract class AbstractWords extends TestCase
-{
-	public void Method(){}
-}
