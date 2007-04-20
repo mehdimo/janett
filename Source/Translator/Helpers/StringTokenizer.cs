993fb34938ebce41d0e2a674faff8afdda76e2c4
edrishn <edrishn@gmail.com>
2007-04-20 08:48:31 +0000

    Mapping more members in java.* packages - Completing StringTokenizer helper
    git-svn-id: https://lpms:444/svn/janett2/trunk@72 b1315daf-0e9f-db11-9a89-001422107726

--- Source/Translator/Helpers/StringTokenizer.cs
+++ Source/Translator/Helpers/StringTokenizer.cs
@@ -1,5 +1,8 @@
 namespace Helpers
 {
+	using System.Collections;
+	using System.Text;
+
 	public class StringTokenizer
 	{
 		private string[] parts;
@@ -7,7 +10,15 @@ namespace Helpers
 
 		public StringTokenizer(string str, string delimiters)
 		{
-			parts = str.Split(delimiters[0]);
+			char[] chars = Encoding.UTF8.GetChars(Encoding.UTF8.GetBytes(delimiters));
+			string[] splitted = str.Split(chars);
+
+			ArrayList list = new ArrayList();
+			for (int i = 0; i < splitted.Length; i++)
+				if (splitted[i] != "")
+					list.Add(splitted[i]);
+
+			parts = (string[]) list.ToArray(typeof(string));
 		}
 
 		public bool hasMoreTokens()
@@ -18,6 +29,11 @@ namespace Helpers
 		public string nextToken()
 		{
 			return parts[i++];
+		}
+
+		public int countTokens()
+		{
+			return parts.Length;
 		}
 	}
 }
