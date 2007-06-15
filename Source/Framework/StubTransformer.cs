c72e59b0aaa4461a72fcba02f787ccff2a4d3544
mehdimo <mehdi.mka@gmail.com>
2007-06-15 15:34:50 +0000

    stubbing methods with adding NotImplentedException (#14)
    git-svn-id: https://lpms:444/svn/janett2/trunk@91 b1315daf-0e9f-db11-9a89-001422107726

--- Source/Framework/StubTransformer.cs
+++ Source/Framework/StubTransformer.cs
@@ -6,22 +6,48 @@ namespace Janett.Framework
 
 	public class StubTransformer : Transformer
 	{
+		private ArrayList stubMembers = new ArrayList();
+
 		public string Inherit;
+		public string Members;
+
+		public bool ShouldStubMember(string name)
+		{
+			return (stubMembers[0].ToString() == "*" || stubMembers.Contains(name));
+		}
 
 		public override object TrackedVisitFieldDeclaration(FieldDeclaration fieldDeclaration, object data)
 		{
-			RemoveCurrentNode();
+			if (ShouldStubMember(((VariableDeclaration) fieldDeclaration.Fields[0]).Name))
+				RemoveCurrentNode();
 			return null;
 		}
 
 		public override object TrackedVisitTypeDeclaration(TypeDeclaration typeDeclaration, object data)
 		{
+			stubMembers.AddRange(Members.Split(','));
 			if (Inherit == null)
 				return base.TrackedVisitTypeDeclaration(typeDeclaration, data);
 			typeDeclaration.Children.Clear();
 			typeDeclaration.BaseTypes.Clear();
 			typeDeclaration.BaseTypes.Add(new TypeReference(Inherit));
 			return null;
+		}
+
+		public override object TrackedVisitConstructorDeclaration(ConstructorDeclaration constructorDeclaration, object data)
+		{
+			if (ShouldStubMember(constructorDeclaration.Name))
+				return base.TrackedVisitConstructorDeclaration(constructorDeclaration, data);
+			else
+				return null;
+		}
+
+		public override object TrackedVisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+		{
+			if (ShouldStubMember(methodDeclaration.Name))
+				return base.TrackedVisitMethodDeclaration(methodDeclaration, data);
+			else
+				return null;
 		}
 
 		public override object TrackedVisitBlockStatement(BlockStatement blockStatement, object data)
